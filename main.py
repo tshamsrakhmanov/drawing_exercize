ebb432800cd4abdf9f0fce42be2f0357d98c01b5
Timur Shmarakhmanov <t.shamsrakhmanov@gmail.com>
2024-12-10 19:20:01 +0400

    argsparse add

--- main.py
+++ main.py
@@ -2,12 +2,14 @@ import settings.resolution
 from engines.DrawingEngine import *
 from engines.ObjectEngine import *
 import pygame
+import argparse
+
+WIDTH = settings.resolution.width
+HEIGHT = settings.resolution.height
 
 
-def main():
+def main(demo_type):
     # 1. resolution setup
-    WIDTH = settings.resolution.width
-    HEIGHT = settings.resolution.height
 
     # 2. pygame setup
     pygame.init()
@@ -44,36 +46,12 @@ def main():
     # 8.1 Static objects declaration - TEST FUNCTION
     objects_buffer = []
 
-    # dot1 = Dot(random.randint(100, WIDTH - 100), random.randint(100, HEIGHT - 100))
-    # i_circ = InteractiveCircle(i_dot=dot1, i_radius=30, i_color=COLOR_WHITE)
-    #
-    # objects_buffer.append(i_circ)
-    #
-    for _ in range(1):
-        dot1 = Dot(random.randint(100, WIDTH - 100), random.randint(100, HEIGHT - 100))
-        dot2 = Dot(random.randint(100, WIDTH - 100), random.randint(100, HEIGHT - 100))
-        dot3 = Dot(random.randint(100, WIDTH - 100), random.randint(100, HEIGHT - 100))
-        dot4 = Dot(random.randint(100, WIDTH - 100), random.randint(100, HEIGHT - 100))
-        i_circ1 = MovableCircle(i_dot=dot1, i_radius=15, i_color=COLOR_WHITE)
-        i_circ2 = MovableCircle(i_dot=dot2, i_radius=15, i_color=COLOR_WHITE)
-        i_circ3 = MovableCircle(i_dot=dot3, i_radius=15, i_color=COLOR_WHITE)
-        i_line1 = Line(dot1, dot2, COLOR_RED, False, COLOR_RED, COLOR_RED)
-        i_line2 = Line(dot2, dot3, COLOR_GREEN, False, COLOR_RED, COLOR_RED)
-        i_line3 = Line(dot1, dot3, COLOR_BLUE, False, COLOR_RED, COLOR_RED)
-        objects_buffer.append(i_circ1)
-        objects_buffer.append(i_circ2)
-        objects_buffer.append(i_circ3)
-        objects_buffer.append(i_line1)
-        objects_buffer.append(i_line2)
-        objects_buffer.append(i_line3)
-
-    # distance = 40
-    #
-    # for i in range(1, 38): # 38
-    #     for y in range(1, 19): # 19
-    #         temp_dot = Dot(distance * i, distance * y)
-    #         objects_buffer.append(
-    #             GradientCircle(i_dot=temp_dot, i_radius=10, i_color=COLOR_WHITE))
+    if demo_type is None:
+        raise Exception('No demo requested')
+    elif demo_type == 'triangle':
+        demo_triangle(objects_buffer)
+    elif demo_type == 'gradient':
+        demo_gradient(objects_buffer)
 
     # 8.2 Test objects transferred to objects engine
     for pos in objects_buffer:
@@ -126,5 +104,40 @@ def main():
         mouse_down = False
 
 
+def demo_triangle(input_buffer):
+    for _ in range(1):
+        dot1 = Dot(random.randint(100, WIDTH - 100), random.randint(100, HEIGHT - 100))
+        dot2 = Dot(random.randint(100, WIDTH - 100), random.randint(100, HEIGHT - 100))
+        dot3 = Dot(random.randint(100, WIDTH - 100), random.randint(100, HEIGHT - 100))
+        dot4 = Dot(random.randint(100, WIDTH - 100), random.randint(100, HEIGHT - 100))
+        i_circ1 = MovableCircle(i_dot=dot1, i_radius=15, i_color=COLOR_WHITE)
+        i_circ2 = MovableCircle(i_dot=dot2, i_radius=15, i_color=COLOR_WHITE)
+        i_circ3 = MovableCircle(i_dot=dot3, i_radius=15, i_color=COLOR_WHITE)
+        i_line1 = Line(dot1, dot2, COLOR_RED, False, COLOR_RED, COLOR_RED)
+        i_line2 = Line(dot2, dot3, COLOR_GREEN, False, COLOR_RED, COLOR_RED)
+        i_line3 = Line(dot1, dot3, COLOR_BLUE, False, COLOR_RED, COLOR_RED)
+        input_buffer.append(i_circ1)
+        input_buffer.append(i_circ2)
+        input_buffer.append(i_circ3)
+        input_buffer.append(i_line1)
+        input_buffer.append(i_line2)
+        input_buffer.append(i_line3)
+
+
+def demo_gradient(input_buffer):
+    distance = 40
+
+    for i in range(1, 38):  # 38
+        for y in range(1, 19):  # 19
+            temp_dot = Dot(distance * i, distance * y)
+            input_buffer.append(
+                GradientCircle(i_dot=temp_dot, i_radius=10, i_color=COLOR_WHITE))
+
+
 if __name__ == '__main__':
-    main()
+    parser = argparse.ArgumentParser(description='Some parser')
+    parser.add_argument('--type', type=str, help='type of demo to show')
+    args = parser.parse_args()
+    # print(args.indir)
+
+    main(args.type)
